49e62e91d01a02cd403a586a6ea4222250c0c83c
Ronan Clarke <rclarke@whatclinic.com>
2013-08-20 21:55:26 +0100

    added contacts and validations

---
+++ app/models/contact.rb
@@ -0,0 +1,25 @@
+class Contact < ActiveRecord::Base
+  attr_accessible :email, :name, :phone
+
+  validates_presence_of :name
+  validates_presence_of :email, :if => "phone.blank?", :message => "Phone or Email must be supplied"
+  validates_presence_of :phone, :if => "email.blank?", :message => "Phone or Email must be supplied"
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :allow_blank => true
+
+
+  def self.generate_test_data
+
+    1000.times do
+
+      Contact.create!({
+                          :name => Faker::Name.name,
+                          :email => Faker::Internet.email,
+                          :phone => Faker::PhoneNumber.phone_number
+                      }
+
+      )
+    end
+  end
+
+
+end
---
+++ db/migrate/20130820200907_add_contact_id_to_appointment.rb
@@ -0,0 +1,5 @@
+class AddContactIdToAppointment < ActiveRecord::Migration
+  def change
+    add_column :appointments, :contact_id, :integer
+  end
+end
